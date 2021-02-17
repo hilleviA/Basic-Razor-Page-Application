8c62c76062cea1b3e38287ab9b3d6a43037b2065
Hillevi Annfält <hillevi.annfalt@hotmail.com>
2021-02-17 23:08:35 +0100

    First Razor Pgae Application

---
+++ RazorPageApp/Startup.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPageApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles(); // För att kunna använda statiska filer som CSS och JavaScript
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
---
+++ RazorPageApp/wwwroot/js/main.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+let button = document.getElementById("navButton");
+let menu = document.getElementById("hiddenNav");
+
+button.onclick = function () {
+
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    }
+    else {
+        menu.style.display = "block";
+    }
+};
